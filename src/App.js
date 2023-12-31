68a13cc549132d0e377eb3bc0d0ad3e6ce7d962e
mfurqanabbas20 <mfurqanabbas20@gmail.com>
2023-12-31 15:13:35 +0500

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import Trees from './Assets/trees.png'
 function App() {
+
+  const[login, setLogin] = React.useState({
+    email: "",
+    password: ""
+  });
+
+  function handleChange(event) {
+    console.log(event);
+    setLogin(prevState => {
+      return {
+        ...prevState,
+      [event.target.name]: event.target.value
+      }
+      
+    }
+      )
+
+  }
+  function getSubmitted() {
+    console.log(login)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='login-main'>
+    <div className='login-content'>
+       <h2>Welcome Back</h2>
+       <p>Enter your credentials to access your account</p>
+    <div className='login-inputs'>
+      <label>Email Address</label>
+      <input 
+      name="email" 
+      type='text' 
+      placeholder='Email address'
+      onChange={handleChange}
+      /> 
+      <label>Password</label>
+      <input 
+      name="password" 
+      type='password' 
+      placeholder='Password'
+      onChange={handleChange}
+      />
+      <button onClick={getSubmitted} className='btn'>Login</button> 
+    </div>   
+
+      </div>
+     <div className='login-image'>
+      <img src={Trees} alt="Tree"/>
+    </div>
     </div>
   );
 }
